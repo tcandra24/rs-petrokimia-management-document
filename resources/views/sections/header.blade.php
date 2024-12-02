6ac5de95db405d8aaffc248bf899e19ec3c98107
Tito Candra <tcandra007@gmail.com>
2024-12-03 05:50:21 +0700

    Add link on logo sidebar

--- resources/views/sections/header.blade.php
+++ resources/views/sections/header.blade.php
@@ -1,7 +1,7 @@
 <header id="header" class="header fixed-top d-flex align-items-center">
 
     <div class="d-flex align-items-center justify-content-between">
-        <a href="index.html" class="logo d-flex align-items-center">
+        <a href="{{ route('dashboard.index') }}" class="logo d-flex align-items-center">
             <img src="{{ asset('assets/img/logo.png') }}" alt="Logo">
         </a>
         <i class="bi bi-list toggle-sidebar-btn"></i>
